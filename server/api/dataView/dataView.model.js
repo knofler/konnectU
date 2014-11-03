984702d683605cd3ec7184d4581f2ef7dad9f5de
Rumman Ahmed <ahmed.rumman@gmail.com>
2014-11-03 17:54:23 +1100

    data added for both public and dev

--- server/api/dataView/dataView.model.js
+++ server/api/dataView/dataView.model.js
@@ -4,9 +4,21 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var DataviewSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+AppsName: String,
+	AppDesc: String,
+	Appversion: String,
+	AppDependency: String,
+	AppOwner: String,
+	LicenseType: String,
+	LicenseCount: Number,
+	LicenseUsed: Number,
+	Department: String,
+	created:Date,
+	AppScope: String,
+	created_by:String,
+	edited:Date,
+	edited_by:String,
+ 	active: Boolean
 });
 
 module.exports = mongoose.model('Dataview', DataviewSchema);
